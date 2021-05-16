32ff137033eb8cb9ff7de91548913dcfc6389b33
Arturo Garcia <rainthstrive@gmail.com>
2021-05-15 23:53:05 -0500

    first commit

---
+++ database/data.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE `demodatabase`;
+
+USE `demodatabase`;
+
+-- Tabla sobre lenguajes de programación
+CREATE TABLE IF NOT EXISTS `demodatabase`.`prog_langs` (
+  `id` INT NOT NULL AUTO_INCREMENT, -- ID de la tabla 
+  `name` VARCHAR(50) NULL, -- Nombre del lenguaje de programación
+  `rel_date` INT NULL, -- Fecha de lanzamiento en formato Unix
+  `auth` VARCHAR(100) NULL, -- Autor del lenguaje
+  `comp` VARCHAR(50) NULL, -- Compañía a la que pertenece
+  PRIMARY KEY (`id`));
