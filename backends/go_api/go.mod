c64e73217d927cfbd2b38271267e14da3dc12c64
Arturo Garcia <rainthstrive@gmail.com>
2021-10-03 21:02:18 -0500

    jwt en uso

--- backends/go_api/go.mod
+++ backends/go_api/go.mod
@@ -3,6 +3,7 @@ module github.com/rainthstrive/app-fullstack-demo
 go 1.15
 
 require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/labstack/echo/v4 v4.3.0
 	github.com/labstack/gommon v0.3.0 // indirect
 	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
--- backends/go_api/main.go
+++ backends/go_api/main.go
@@ -4,7 +4,9 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 
+	"github.com/golang-jwt/jwt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -17,11 +19,48 @@ type (
 		Auth string `json:"auth"`
 		Comp string `json:"comp"`
 	}
+	jwtCustomClaims struct {
+		Name  string `json:"name"`
+		Admin bool   `json:"admin"`
+		jwt.StandardClaims
+	}
 )
 
 //----------
 // Handlers
 //----------
+
+func login(c echo.Context) error {
+	username := c.FormValue("username")
+	password := c.FormValue("password")
+
+	// Throws unauthorized error
+	if username != "jon" || password != "shhh!" {
+		return echo.ErrUnauthorized
+	}
+
+	// Set custom claims
+	claims := &jwtCustomClaims{
+		"Jon Snow",
+		true,
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * 72).Unix(),
+		},
+	}
+
+	// Create token with claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// Generate encoded token and send it as response.
+	t, err := token.SignedString([]byte("gobhgb76/&Jngnghn"))
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{
+		"token": t,
+	})
+}
 
 func createLang(c echo.Context) error {
 	lang := new(prog_langs)
@@ -116,14 +155,26 @@ func main() {
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"http://localhost:4200", "http://localhost:3100"},
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
-	  }))
+	  })) 
 
 	// Routes
 	e.GET("/prog_langs", getAllLangs)
-	e.POST("/prog_langs", createLang)
 	e.GET("/prog_langs/:id", getLang)
-	e.PUT("/prog_langs/:id", updateUser)
-	e.DELETE("/prog_langs/:id", deleteUser)
+	// Login route
+	e.POST("/login", login)
+
+	// Restricted group
+	r := e.Group("/restricted")
+
+	config := middleware.JWTConfig{
+		Claims:     &jwtCustomClaims{},
+		SigningKey: []byte("gobhgb76/&Jngnghn"),
+	}
+
+	r.Use(middleware.JWTWithConfig(config))
+	r.POST("/prog_langs", createLang)
+	r.PUT("/prog_langs/:id", updateUser)
+	r.DELETE("/prog_langs/:id", deleteUser)
 
 	// Start server
 	e.Logger.Fatal(e.Start("localhost:1323"))
